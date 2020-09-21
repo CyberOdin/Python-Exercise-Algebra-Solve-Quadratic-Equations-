d5bde4699f19ff6c248f3799a6fea3eec4011ce9
Andre Ashmeade <55720341+CyberOdin@users.noreply.github.com>
2020-09-21 09:48:33 -0400

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,8 +1,23 @@
+""" Solving Quadratic Equations"""
+"""
+The two roots of a quadratic equation ax^2 + bx + c = 0 can be obtained using the following formula:
+b^2 - 4ac is called the discriminant of the quadratic equation. If it is positive, the equation has two real roots.
+If it is zero, the equation has one root. If it is negative, the equation has no real roots.
+Write a program that prompts the user to enter values for a, b, and c and displays the result based on the discriminant.
+
+If the discriminant is positive, display two roots.
+
+If the discriminant is 0, display one root.
+
+Otherwise, display “The equation has no real roots”.
+"""
+
+
 import math
 
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-c = int(input("Enter c: "))
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
 
 d = (b**2) - (4*a*c)
 
